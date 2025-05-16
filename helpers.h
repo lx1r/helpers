175c3f3804b919919c653e6eb3ad35a306b29d47
marvin <lx1r@github.com>
2025-05-16 21:14:30 +0300

    Cleanup

--- helpers.h
+++ helpers.h
@@ -11,7 +11,7 @@
 #define ___nth(_, _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, N, ...) N
 #endif
 #ifndef ___narg
-#define ___narg(...) \
+#define ___narg(...)\
 	___nth(_, ##__VA_ARGS__, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 #endif
 
@@ -30,11 +30,11 @@
 #define ___fill12(arr, p, x, ...) arr[p] = x; ___fill4(arr, p + 1, __VA_ARGS__)
 
 #define ___fill(arr, ...)\
-        ___apply(___fill, ___narg(__VA_ARGS__))(arr, 0, ##__VA_ARGS__)
+	___apply(___fill, ___narg(__VA_ARGS__))(arr, 0, ##__VA_ARGS__)
 
 #define ___fill_shift(arr, ...) ({\
-        ___fill(arr, __VA_ARGS__);\
-        (arr) += ___narg(__VA_ARGS__);\
+	___fill(arr, __VA_ARGS__);\
+	(arr) += ___narg(__VA_ARGS__);\
 })
 
 static inline void ___pfree(void *pptr) { free(*(void **)pptr); }
@@ -42,23 +42,23 @@ static inline void ___pfree(void *pptr) { free(*(void **)pptr); }
 
 static inline size_t ___align_sz(size_t nb)
 {
-        if (!nb) return 0;
-        /* initial size */
-        if (!(nb & ~0xf))
-                return 64;
-        /* align to page */
-        if (nb & ~0xfff)
-                return (nb + 0xfff) & ~0xfff;
-        /* align to power of 2 */
-        nb = nb - 1;
-        nb |= (nb >> 1);
-        nb |= (nb >> 2);
-        nb |= (nb >> 4);
-        return nb + 1;
+	if (!nb) return 0;
+	/* initial size */
+	if (!(nb & ~0xf))
+		return 64;
+	/* align to page */
+	if (nb & ~0xfff)
+		return (nb + 0xfff) & ~0xfff;
+	/* align to power of 2 */
+	nb = nb - 1;
+	nb |= (nb >> 1);
+	nb |= (nb >> 2);
+	nb |= (nb >> 4);
+	return nb + 1;
 }
 
 struct ___meta {
-        size_t len;
+	size_t len;
 };
 
 #define ___cap(ptr) malloc_usable_size(ptr)
@@ -72,32 +72,32 @@ struct ___meta {
 #define ___extend(ptr, len) ptr = realloc(ptr, ___align_sz((len)*sizeof(*(ptr)) + ___meta_sz))
 
 #define append(pptr, ...) ({\
-        size_t ___len = len(*(pptr));\
-        ___extend(*(pptr), ___len + ___narg(__VA_ARGS__));\
-        ___fill((*(pptr) + ___len), ##__VA_ARGS__);\
-        ___len(*(pptr)) = ___len + ___narg(__VA_ARGS__);\
+	size_t ___len = len(*(pptr));\
+	___extend(*(pptr), ___len + ___narg(__VA_ARGS__));\
+	___fill((*(pptr) + ___len), ##__VA_ARGS__);\
+	___len(*(pptr)) = ___len + ___narg(__VA_ARGS__);\
 })
 
 #define ___fill_pr_fmt(arr, x)\
-        _Generic(x,\
-                 _Bool:                 ___fill_shift(arr, '%', 'd'),\
-                 char:                  ___fill_shift(arr, '%', 'c'),\
-                 signed char:           ___fill_shift(arr, '%', 'h', 'h', 'i'),\
-                 unsigned char:         ___fill_shift(arr, '%', 'h', 'h', 'u'),\
-                 signed short:          ___fill_shift(arr, '%', 'h', 'i'),\
-                 unsigned short:        ___fill_shift(arr, '%', 'h', 'u'),\
-                 signed int:            ___fill_shift(arr, '%', 'i'),\
-                 unsigned int:          ___fill_shift(arr, '%', 'u'),\
-                 signed long:           ___fill_shift(arr, '%', 'l', 'i'),\
-                 unsigned long:         ___fill_shift(arr, '%', 'l', 'u'),\
-                 signed long long:      ___fill_shift(arr, '%', 'l', 'l', 'i'),\
-                 unsigned long long:    ___fill_shift(arr, '%', 'l', 'l', 'u'),\
-                 float:                 ___fill_shift(arr, '%', 'f'),\
-                 double:                ___fill_shift(arr, '%', 'f'),\
-                 long double:           ___fill_shift(arr, '%', 'L', 'f'),\
-                 char *:                ___fill_shift(arr, '%', 's'),\
-                 const char *:          ___fill_shift(arr, '%', 's'),\
-                 default:               ___fill_shift(arr, '%', 'p'))
+	_Generic(x,\
+		 _Bool:                 ___fill_shift(arr, '%', 'd'),\
+		 char:                  ___fill_shift(arr, '%', 'c'),\
+		 signed char:           ___fill_shift(arr, '%', 'h', 'h', 'i'),\
+		 unsigned char:         ___fill_shift(arr, '%', 'h', 'h', 'u'),\
+		 signed short:          ___fill_shift(arr, '%', 'h', 'i'),\
+		 unsigned short:        ___fill_shift(arr, '%', 'h', 'u'),\
+		 signed int:            ___fill_shift(arr, '%', 'i'),\
+		 unsigned int:          ___fill_shift(arr, '%', 'u'),\
+		 signed long:           ___fill_shift(arr, '%', 'l', 'i'),\
+		 unsigned long:         ___fill_shift(arr, '%', 'l', 'u'),\
+		 signed long long:      ___fill_shift(arr, '%', 'l', 'l', 'i'),\
+		 unsigned long long:    ___fill_shift(arr, '%', 'l', 'l', 'u'),\
+		 float:                 ___fill_shift(arr, '%', 'f'),\
+		 double:                ___fill_shift(arr, '%', 'f'),\
+		 long double:           ___fill_shift(arr, '%', 'L', 'f'),\
+		 char *:                ___fill_shift(arr, '%', 's'),\
+		 const char *:          ___fill_shift(arr, '%', 's'),\
+		 default:               ___fill_shift(arr, '%', 'p'))
 
 #define ___fill_fmt0(arr, x)
 #define ___fill_fmt1(arr, x) ___fill_pr_fmt(arr, x)
@@ -114,67 +114,67 @@ struct ___meta {
 #define ___fill_fmt12(arr, x, ...) ___fill_pr_fmt(arr, x); ___fill_fmt11(arr, __VA_ARGS__)
 
 #define ___fill_fmt(arr, ...)\
-        ___apply(___fill_fmt, ___narg(__VA_ARGS__))(arr, __VA_ARGS__)
+	___apply(___fill_fmt, ___narg(__VA_ARGS__))(arr, __VA_ARGS__)
 
 #define fprint(fp, ...) ({\
-        char ___fmt[___narg(__VA_ARGS__)*4 + 1];\
-        char *___dst = ___fmt;\
-        ___fill_fmt(___dst, __VA_ARGS__);\
-        *___dst++ = '\0';\
-        fprintf(fp, ___fmt, ##__VA_ARGS__);\
+	char ___fmt[___narg(__VA_ARGS__)*4 + 1];\
+	char *___dst = ___fmt;\
+	___fill_fmt(___dst, __VA_ARGS__);\
+	*___dst++ = '\0';\
+	fprintf(fp, ___fmt, ##__VA_ARGS__);\
 })
 
 #define print(...) fprint(stdout, ##__VA_ARGS__)
 
 #define fprintln(fp, ...) ({\
-        char ___fmt[___narg(__VA_ARGS__)*4 + 2];\
-        char *___dst = ___fmt;\
-        ___fill_fmt(___dst, __VA_ARGS__);\
-        *___dst++ = '\n';\
-        *___dst++ = '\0';\
-        fprintf(fp, ___fmt, ##__VA_ARGS__);\
+	char ___fmt[___narg(__VA_ARGS__)*4 + 2];\
+	char *___dst = ___fmt;\
+	___fill_fmt(___dst, __VA_ARGS__);\
+	*___dst++ = '\n';\
+	*___dst++ = '\0';\
+	fprintf(fp, ___fmt, ##__VA_ARGS__);\
 })
 
 #define println(...) fprintln(stdout, ##__VA_ARGS__)
 
 #define join(...) ({\
-        char ___fmt[___narg(__VA_ARGS__)*4 + 1];\
-        char *___dst = ___fmt;\
-        ___fill_fmt(___dst, __VA_ARGS__);\
-        *___dst++ = '\0';\
-        size_t nb = snprintf(NULL, 0, ___fmt, __VA_ARGS__);\
-        char *buf = malloc(nb + 1);\
-        if (buf) sprintf(buf, ___fmt, __VA_ARGS__);\
-        buf;\
+	char ___fmt[___narg(__VA_ARGS__)*4 + 1];\
+	char *___dst = ___fmt;\
+	___fill_fmt(___dst, __VA_ARGS__);\
+	*___dst++ = '\0';\
+	size_t nb = snprintf(NULL, 0, ___fmt, __VA_ARGS__);\
+	char *buf = malloc(nb + 1);\
+	if (buf) sprintf(buf, ___fmt, __VA_ARGS__);\
+	buf;\
 })
 #define ___strto(x, s) ({\
-        const char *str = s;\
-        (str) ? \
-        _Generic(x,\
-                 _Bool:                 strcasecmp(str, "true") == 0 || strtoul(str, NULL, 0),\
-                 char:                  str[0],\
-                 signed char:           (signed char)strtol(str, NULL, 0),\
-                 unsigned char:         (unsigned char)strtoul(str, NULL, 0),\
-                 signed short:          (signed short)strtol(str, NULL, 0),\
-                 unsigned short:        (unsigned short)strtoul(str, NULL, 0),\
-                 signed int:            (signed int)strtol(str, NULL, 0),\
-                 unsigned int:          (unsigned int)strtoul(str, NULL, 0),\
-                 signed long:           strtol(str, NULL, 0),\
-                 unsigned long:         strtoul(str, NULL, 0),\
-                 signed long long:      strtoll(str, NULL, 0),\
-                 unsigned long long:    strtoull(str, NULL, 0),\
-                 float:                 strtof(str, NULL),\
-                 double:                strtod(str, NULL),\
-                 long double:           strtold(str, NULL),\
-                 char *:                strdup(str)) : 0;\
+	const char *str = s;\
+	(str) ? \
+	_Generic(x,\
+		 _Bool:                 strcasecmp(str, "true") == 0 || strtoul(str, NULL, 0),\
+		 char:                  str[0],\
+		 signed char:           (signed char)strtol(str, NULL, 0),\
+		 unsigned char:         (unsigned char)strtoul(str, NULL, 0),\
+		 signed short:          (signed short)strtol(str, NULL, 0),\
+		 unsigned short:        (unsigned short)strtoul(str, NULL, 0),\
+		 signed int:            (signed int)strtol(str, NULL, 0),\
+		 unsigned int:          (unsigned int)strtoul(str, NULL, 0),\
+		 signed long:           strtol(str, NULL, 0),\
+		 unsigned long:         strtoul(str, NULL, 0),\
+		 signed long long:      strtoll(str, NULL, 0),\
+		 unsigned long long:    strtoull(str, NULL, 0),\
+		 float:                 strtof(str, NULL),\
+		 double:                strtod(str, NULL),\
+		 long double:           strtold(str, NULL),\
+		 char *:                strdup(str)) : 0;\
 })
 
 #define splitv(str, delim, pptr) ({\
-        char *dup = strdup(str);\
-        for (char *token = strtok(dup, delim); token; token = strtok(NULL, delim)) {\
-                append(pptr, ___strto(**(pptr), token));\
-        }\
-        free(dup);\
+	char *dup = strdup(str);\
+	for (char *token = strtok(dup, delim); token; token = strtok(NULL, delim)) {\
+		append(pptr, ___strto(**(pptr), token));\
+	}\
+	free(dup);\
 })
 
 #define ___splitn(str, delim, p) *(p) = ___strto(*(p), strtok(NULL, delim))
@@ -190,11 +190,12 @@ struct ___meta {
 #define ___split10(str, delim, p, ...) ___splitn(str, delim, p); ___split9(str, delim, __VA_ARGS__)
 #define ___split11(str, delim, p, ...) ___splitn(str, delim, p); ___split10(str, delim, __VA_ARGS__)
 #define ___split12(str, delim, p, ...) ___splitn(str, delim, p); ___split11(str, delim, __VA_ARGS__)
+
 #define split(str, delim, p, ...) ({\
-        char *dup = strdup(str);\
-        *(p) = ___strto(*(p), strtok(dup, delim));\
-        ___apply(___split, ___narg(p, __VA_ARGS__))(dup, delim, ##__VA_ARGS__);\
-        free(dup);\
+	char *dup = strdup(str);\
+	*(p) = ___strto(*(p), strtok(dup, delim));\
+	___apply(___split, ___narg(p, __VA_ARGS__))(dup, delim, ##__VA_ARGS__);\
+	free(dup);\
 })
 
 #define ___decl1(x) x;
@@ -211,6 +212,6 @@ struct ___meta {
 #define ___decl12(x, ...) x; ___decl11(__VA_ARGS__)
 
 #define func(args, expr)\
-        ({ typeof(({ ___apply(___decl, ___narg args) args expr; })) ___func args { return expr; } ___func; })
+	({ typeof(({ ___apply(___decl, ___narg args) args expr; })) ___func args { return expr; } ___func; })
 
 #endif
