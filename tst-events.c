1340a6cba627ed909448a3c7dc4c1180e1f11c10
marvin <lx11rv@gmail.com>
2025-12-04 16:18:32 +0300

    Create tst-events.c

---
+++ tst-events.c
@@ -0,0 +1,92 @@
+#include "helpers.h"
+
+int *event_list = NULL;
+pair(int, int) *event_map = NULL;
+
+static inline int event(void)
+{
+	static unsigned seed = 31421;
+	seed = 16807*(seed%127773) - 2836*(seed/127773);
+	return seed & 0xffff;
+}
+
+void dump_events(void)
+{
+	println("len=", len(event_map));
+	foreach (ref, event_map)
+		println("slot=", ref - event_map, " ev=", ref->key, " count=", ref->value);
+}
+
+void register_event(void)
+{
+	int ev = event();
+	static int count = 0;
+
+	int *data = lookup(&event_map, ev);
+	if (data) {
+		println("event ", ev, " already registered");
+		return;
+	}
+	data = insert(&event_map, ev, count++);
+	println("registered event ", ev, " count=", *data);
+	append(&event_list, ev);
+}
+
+void unregister_event(int ev)
+{
+	int *data = lookup(&event_map, ev);
+	if (!data) {
+		println("event ", ev, " not registered");
+		dump_events();
+		return;
+	}
+	int count = *data;
+	delete(&event_map, data);
+	println("unregistered event ", ev, " count=", count);
+}
+
+void find_event(int ev)
+{
+	int *data = lookup(&event_map, ev);
+	if (!data) {
+		println("event ", ev, " not found");
+		dump_events();
+		return;
+	}
+	println("found event ", ev, " count=", *data);
+}
+
+void sanity(void)
+{
+	void *lookup_ret = lookup(&event_map, 11);
+	println("lookup ret: ", lookup_ret);
+	ssize_t delete_ret = delete(&event_map, event_map + 11);
+	println("delete ret: ", delete_ret);
+}
+
+int main()
+{
+	sanity();
+
+	for (int i = 0; i < 10000; i++)
+		register_event();
+
+	for (int i = 0; i < len(event_list); i++)
+		find_event(event_list[i]);
+
+	//dump_events();
+
+	for (int i = 0; i < len(event_list)/8; i++)
+		unregister_event(event_list[i]);
+
+	for (int i = len(event_list)/8; i < len(event_list); i++)
+		find_event(event_list[i]);
+
+	println("len(event_map)=", len(event_map));
+	println("count(event_map)=", count(event_map));
+	println("len(event_list)=", len(event_list));
+
+	println("probes per lookup: ", ___lookup_probes/(double)___lookups);
+
+	return 0;
+}
